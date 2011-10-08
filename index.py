b4230be540d3eaa562ebb3edbc2d21a443b08a53
Vipin Nair <swvist@gmail.com>
2011-10-09 03:10:52 +0530

    Question data submission and question list

--- index.py
+++ index.py
@@ -1,5 +1,7 @@
 import os
 import time
+import string
+import questiondb
 import simplejson as json
 from google.appengine.ext.webapp import template
 from google.appengine.api import users
@@ -37,14 +39,38 @@ class admin(webapp.RequestHandler):
 
 class adminSubmitQuestions(webapp.RequestHandler):
 	def post(self):
-		self.response.out.write(self.request.get('ques'))
+		q = questiondb.question(questionNumber=string.atoi(self.request.get('qno')),
+								question=self.request.get('ques'),
+								qimage=self.request.get('qimg'),
+								opt1=self.request.get('opt1'),
+								opt2=self.request.get('opt2'),
+								opt3=self.request.get('opt3'),
+								opt4=self.request.get('opt4'),
+								ans=self.request.get('ans'))
+		q.put()
+
+class adminListQuestions(webapp.RequestHandler):
+	def get(self):
+		self.response.headers['Content-Type'] = 'text/html'
+		query = questiondb.question.all()
+		result = query.fetch(5)
+		for q in result:
+			self.response.out.write(q.questionNumber)
+			self.response.out.write(q.question+"<br />")
+			self.response.out.write(q.qimage+"<br />")
+			self.response.out.write(q.opt1+"<br />")
+			self.response.out.write(q.opt2+"<br />")
+			self.response.out.write(q.opt3+"<br />")
+			self.response.out.write(q.opt4+"<br />")
+			self.response.out.write(q.ans+"<br />")
 
 application = webapp.WSGIApplication(
 									[('/', index),
 									('/contest/start(|/)',contestStart),
 									('/contest/question/(\d)/', contestQuestion),
 									('/admin/', admin),
-									('/admin/submit/questions/', adminSubmitQuestions)],
+									('/admin/submit/questions/', adminSubmitQuestions),
+									('/admin/list/questions/',adminListQuestions)],
 									debug=True)
 
 def main():
