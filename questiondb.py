3bec8a8e1712b2e31a9c7067ac9955f987718ad7
Vipin Nair <swvist@gmail.com>
2011-10-09 03:02:02 +0530

    Added answer field to question model

--- questiondb.py
+++ questiondb.py
@@ -1,6 +1,6 @@
 # Model Defining Questions Database
 from google.appengine.ext import db
-class question(db.model):
+class question(db.Model):
 	questionNumber = db.IntegerProperty(required=True)
 	question = db.StringProperty(required=True)
 	qimage = db.StringProperty(required=True)
@@ -8,4 +8,5 @@ class question(db.model):
 	opt2 = db.StringProperty(required=True)
 	opt3 = db.StringProperty(required=True)
 	opt4 = db.StringProperty(required=True)
+	ans = db.StringProperty(required=True)
 	
