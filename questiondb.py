c50dee43fe037685e8c9d3ead8c4166557ea57a5
Kartik Singhal <kartiksinghal@gmail.com>
2011-10-08 22:19:09 +0530

    Removed extra db. in opt 1

--- questiondb.py
+++ questiondb.py
@@ -4,7 +4,7 @@ class question(db.model):
 	questionNumber = db.IntegerProperty(required=True)
 	question = db.StringProperty(required=True)
 	qimage = db.StringProperty(required=True)
-	opt1 = db.db.StringProperty(required=True)
+	opt1 = db.StringProperty(required=True)
 	opt2 = db.StringProperty(required=True)
 	opt3 = db.StringProperty(required=True)
 	opt4 = db.StringProperty(required=True)
