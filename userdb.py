aed120a4995e9c13d021d882b385290e4a810470
Vipin Nair <swvist@gmail.com>
2011-10-10 02:53:06 +0530

    User details and answer database and other functions

---
+++ userdb.py
@@ -0,0 +1,37 @@
+#User operations and database
+
+import random
+import string
+from google.appengine.api import users
+from google.appengine.ext import db
+from google.appengine.api import users
+
+def generateSet():
+	a = []
+	for i in range(45):
+		a.append(i+1)
+	random.shuffle(a)
+	return a
+
+
+class userPlay(db.Model):
+	user = db.UserProperty(required=True)
+	questionSet = db.ListProperty(int)
+	tathvaID = db.StringProperty(required=True)
+	startTime = db.DateTimeProperty(required=True)
+	endTime = db.DateTimeProperty
+
+def userPlayInsert(tid,stime):
+	u = userPlay(user = users.get_current_user(),
+				questionSet = generateSet(),
+				tathvaID = tid,
+				startTime = stime)
+	u.put()
+	return u.user.nickname() + u.tathvaID
+	
+class userAnswers(db.Model):
+	user = user = db.UserProperty(required=True)
+	question = db.IntegerProperty(required=True)
+	answer = db.StringProperty(required=True)
+	elapsedTime = db.DateTimeProperty(required=True)
+
