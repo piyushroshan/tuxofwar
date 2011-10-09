fe5aeb9972414388a2098a100916b64f199f1b52
Jaseem Abid <jaseemabid@gmail.com>
2011-10-09 22:30:59 +0530

    do nothing for unknown commands

--- js/xkcd_cli.js
+++ js/xkcd_cli.js
@@ -612,7 +612,6 @@ TerminalShell.fallback = function(terminal, cmd) {
 		} else if (/:\(\)\s*{\s*:\s*\|\s*:\s*&\s*}\s*;\s*:/.test(cmd)) {
 			Terminal.setWorking(true);
 		} else {
-			$.get("/unixkcd/missing", {cmd: cmd});
 			return false;
 		}
 	}
