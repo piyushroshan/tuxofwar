f5b5f432453e67ce4af7f24b30f70f518f29dabc
Vipin Nair <swvist@gmail.com>
2011-10-10 18:04:12 +0530

    Added admin check to all /admin/* paths

--- index.py
+++ index.py
@@ -45,50 +45,81 @@ class contestAnswer(webapp.RequestHandler):
 
 class adminQuestionsAdd(webapp.RequestHandler):
 	def get(self):
-		self.response.headers['Content-Type'] = 'text/html'
-		path = os.path.join(os.path.dirname(__file__), 'templates/question.html')
-		self.response.out.write(template.render(path,""))
+		if users.get_current_user():
+			if users.is_current_user_admin():
+				self.response.headers['Content-Type'] = 'text/html'
+				path = os.path.join(os.path.dirname(__file__), 'templates/question.html')
+				self.response.out.write(template.render(path,""))
+			else:
+				print "Not allowed!"
+		else:
+			self.redirect(users.create_login_url(self.request.uri))
+	
 
 class adminQuestionsSubmit(webapp.RequestHandler):
 	def post(self):
-		q = questiondb.question(questionNumber=string.atoi(self.request.get('qno')),
-								question=self.request.get('ques'),
-								qimage=self.request.get('qimg'),
-								opt1=self.request.get('opt1'),
-								opt2=self.request.get('opt2'),
-								opt3=self.request.get('opt3'),
-								opt4=self.request.get('opt4'),
-								ans=self.request.get('ans'))
-		q.put()
-		self.redirect('/admin/questions/list/')
+		if users.get_current_user():
+			if users.is_current_user_admin():
+				q = questiondb.question(questionNumber=string.atoi(self.request.get('qno')),
+										question=self.request.get('ques'),
+										qimage=self.request.get('qimg'),
+										opt1=self.request.get('opt1'),
+										opt2=self.request.get('opt2'),
+										opt3=self.request.get('opt3'),
+										opt4=self.request.get('opt4'),
+										ans=self.request.get('ans'))
+				q.put()
+				self.redirect('/admin/questions/list/')
+			else:
+				print "Not allowed!"
+		else:
+			self.redirect(users.create_login_url(self.request.uri))
 
 class adminQuestionsList(webapp.RequestHandler):
 	def get(self):
-		self.response.headers['Content-Type'] = 'text/html'
-		query = questiondb.question.all()
-		result = query.fetch(5)
-		template_values = { 'questions' : result }
-		path = os.path.join(os.path.dirname(__file__), 'templates/questionlist.html')
-		self.response.out.write(template.render(path, template_values))
+		if users.get_current_user():
+			if users.is_current_user_admin():
+				self.response.headers['Content-Type'] = 'text/html'
+				query = questiondb.question.all()
+				result = query.fetch(5)
+				template_values = { 'questions' : result }
+				path = os.path.join(os.path.dirname(__file__), 'templates/questionlist.html')
+				self.response.out.write(template.render(path, template_values))
+			else:
+				print "Not allowed!"
+		else:
+			self.redirect(users.create_login_url(self.request.uri))
 
 class adminQuestionsAnswered(webapp.RequestHandler):
 	def get(self):
-		self.response.headers['Content-Type'] = 'text/html'
-		query = userdb.userAnswer.all().order('elapsedTime')
-		result = query.fetch(100)
-		for ans in result:
-			self.response.out.write(ans.user.nickname()+str(ans.question)+
-									ans.answer+str(ans.elapsedTime)+"<br />")
+		if users.get_current_user():
+			if users.is_current_user_admin():
+				self.response.headers['Content-Type'] = 'text/html'
+				query = userdb.userAnswer.all().order('elapsedTime')
+				result = query.fetch(100)
+				for ans in result:
+					self.response.out.write(ans.user.nickname()+str(ans.question)+
+											ans.answer+str(ans.elapsedTime)+"<br />")
+			else:
+				print "Not allowed!"
+		else:
+			self.redirect(users.create_login_url(self.request.uri))
 
 class adminContestUsers(webapp.RequestHandler):
-	def get(self):
-		self.response.headers['Content-Type'] = 'text/html'
-		query = userdb.userPlay.all().order('startTime')
-		result = query.fetch(100)
-		for ans in result:
-			self.response.out.write(ans.user.nickname()+ans.tathvaID+"<br />")
-			self.response.out.write(ans.questionSet)
-			self.response.out.write("<br />")
+	if users.get_current_user():
+		if users.is_current_user_admin():
+			def get(self):
+				self.response.headers['Content-Type'] = 'text/html'
+				query = userdb.userPlay.all().order('startTime')
+				result = query.fetch(100)
+				for ans in result:
+					self.response.out.write(ans.user.nickname()+ans.tathvaID+"<br />")
+					self.response.out.write(ans.questionSet)
+					self.response.out.write("<br />")
+		else:
+				print "Not allowed!"
+	else:
+		self.redirect(users.create_login_url(self.request.uri))
 
 application = webapp.WSGIApplication(
 									[('/', index),
